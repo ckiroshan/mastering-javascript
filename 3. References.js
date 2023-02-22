71f3835f35f315c03323118b3d89711e3287237e
ckiroshan <ckiroshan@gmail.com>
2023-02-22 11:43:52 +0530

    Added Arrays.

--- 3. References.js
+++ 3. References.js
@@ -15,6 +15,23 @@ console.log(person.name); // Bryan
 
 // Bracket Notation
 person["name"] = "Mary";
-console.log(person['name']); // Mary
+console.log(person["name"]); // Mary
 
+// <================= Arrays =================>
 
+let selectedColor = ["red", "white"];
+console.log(selectedColor); // (2) ['red', 'white']
+
+// each element in a array can be accessed using index.
+// Index:    0  |  1
+//         red  |  white
+
+console.log(selectedColor[0]); // red
+
+selectedColor[2] = "green";
+console.log(selectedColor); // (3) ['red', 'white', 'green']
+
+selectedColor[3] = 4;
+console.log(selectedColor); // (4) ['red', 'white', 'green', 4]
+
+console.log(selectedColor.length) // 4
