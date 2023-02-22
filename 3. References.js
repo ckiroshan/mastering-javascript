8139d027a8f8e9f6e875b8cafd4e263c5d60e84e
ckiroshan <ckiroshan@gmail.com>
2023-02-22 11:26:28 +0530

    Added Objects.

---
+++ 3. References.js
@@ -0,0 +1,20 @@
+// <================= Objects =================>
+
+let person = {
+  name: "Kane",
+  age: 21,
+}; // Object literal
+
+console.log(person);
+
+// the value of a property can be changed in 2 ways.
+
+// Dot Notation
+person.name = "Bryan";
+console.log(person.name); // Bryan
+
+// Bracket Notation
+person["name"] = "Mary";
+console.log(person['name']); // Mary
+
+
