9d552c3866d5a9968c13a2d8b61a8e1c87d9d137
ckiroshan <ckiroshan@gmail.com>
2023-02-22 10:51:07 +0530

    Added primitive data types.

---
+++ 2. Primitives.js
@@ -0,0 +1,5 @@
+let name = "Kane"; // String literal
+let age = 21; // Number literal
+let isApproved = true; // Boolean literal
+let firstName; // undefined
+let lastName = null; // used to clear value of a variable.
