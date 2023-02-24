759acb39cfe247321d62a08281f76c2aa4879b15
ckiroshan <ckiroshan@gmail.com>
2023-02-24 08:30:05 +0530

    Added assignment operators.

---
+++ 4. Operators.js
@@ -0,0 +1,36 @@
+// <================= Arithmetic Operators =================>
+
+let x = 4;
+let y = 10;
+
+console.log(x + y); // Addition operator
+// 'x' & 'y' are operands.
+// 'x + y' is a an expression in JS
+
+console.log(x + y); // Addition operator
+console.log(x - y); // Substraction operator
+console.log(x * y); // Multiplication operator
+console.log(x / y); // Division operator
+console.log(x % y); // Modulas operator (remainder of division)
+console.log(x ** y); // Exponentiation operator (X to power of Y)
+
+// <==== Increment (++) ====>
+x = 20;
+console.log(++x); // 21
+// '++' before x would increment x first and log it to console.
+
+console.log(x++); //21
+// '++' after x would log to console first. then it would be incremented.
+console.log(x); // 22
+// Now the value is incremented.
+
+// <==== Decrement (--) ====>
+// same applies to decrement as well
+
+x = 20;
+
+console.log(--x); // 19
+console.log(x--); // 19
+console.log(x); // 18
+console.log("");
+
