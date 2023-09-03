fc3b42c211e249362d0fe410c7367979dc335579
Your <ckiroshan@gmail.com>
2023-09-03 21:26:20 +0530

    do-while loop in JS.

---
+++ 9. do-while-loop.js
@@ -0,0 +1,12 @@
+console.log("<=== Do-While loops in JS ===>");
+
+console.log("");
+
+// Even Numbers
+console.log("All Even numbers upto 10");
+let i = 1;
+do {
+  if (i % 2 == 0) console.log("Even Number:", i);
+  i++;
+}
+while (i <= 10) 
