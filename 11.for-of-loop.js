37fe0a6e3313082f9c5c49519d8672ce3ad137f5
Your <ckiroshan@gmail.com>
2023-09-06 17:29:34 +0530

    For-of loop in JS.

---
+++ 11.for-of-loop.js
@@ -0,0 +1,10 @@
+console.log("<=== For of loops in JS ===>");
+console.log("");
+
+const colors = ["Green", "Black", "Blue"];
+
+for (let color of colors) {
+  console.log(color);
+  // Output: Green, Black, Blue
+}
+
