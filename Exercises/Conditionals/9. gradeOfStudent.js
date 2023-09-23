769ceb11310a2786afb096bb2278ea789cdab8aa
Your <ckiroshan@gmail.com>
2023-09-23 17:59:55 +0530

    Exercise: Calculate Grade Of a Student (2).

--- Exercises/Conditionals/9. gradeOfStudent.js
+++ Exercises/Conditionals/9. gradeOfStudent.js
@@ -5,7 +5,6 @@ const marks = [80, 50, 80];
 
 console.log(calculateGrade(marks));
 
-
 // Approach #1
 function calculateGrade(marks) {
   // calculate avg grade
@@ -25,3 +24,33 @@ function calculateGrade(marks) {
   if (averageGrade < 89) return "B";
   return "A";
 }
+console.log("");
+
+// Approach #2
+// Create 2 functions this time.
+
+function calculateAverage(array) {
+  let total = 0;
+  let average = 0;
+
+  for (let value of array) {
+    total += value;
+  }
+  average = total / array.length;
+  console.log("Array: ", array);
+  console.log("Total: ", total);
+  console.log("Average:", average);
+  return average;
+}
+
+function calculateGrade2(marks) {
+  const average = calculateAverage(marks);
+  // console.log("Average Marks:", averageMarks);
+  if (average < 60) return "F";
+  else if (average < 69) return "D";
+  else if (average < 79) return "C";
+  else if (average < 89) return "B";
+  else return "A";
+}
+
+console.log(calculateGrade2(marks));
