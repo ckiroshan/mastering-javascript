fd32a17877fcc5d75647b8b2e33fb9b30210af59
Your <ckiroshan@gmail.com>
2024-04-25 17:26:53 +0530

    Template literals in Javascript.

---
+++ Objects/10. Template-literals.js
@@ -0,0 +1,10 @@
+const name = "John";
+
+const normalString = "Hey there " + name + "!";
+
+console.log(normalString);
+
+// Template literals
+
+const templateString = `Hey there ${name}!`;
+console.log(templateString);
