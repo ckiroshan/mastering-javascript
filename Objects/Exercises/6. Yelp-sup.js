44491bfc23f8aef56412bef6f1de8b46f875cf53
Your <ckiroshan@gmail.com>
2024-04-26 19:56:38 +0530

    Exercise: Post Object using constructor

---
+++ Objects/Exercises/6. Yelp-sup.js
@@ -0,0 +1,31 @@
+console.log("<=========== Ex Yelp Sup ===========>");
+
+/**
+ * Create an array of 3 objects.
+ * Each object represents a price range.
+ * Think about the properties each object should have.
+ */
+
+const priceRange = [
+  {
+    label: "$",
+    tooltip: "inexpensive",
+    minPerPerson: 1,
+    maxPerPerson: 10,
+  },
+  {
+    label: "$$",
+    tooltip: "moderate",
+    minPerPerson: 11,
+    maxPerPerson: 20,
+  },
+  {
+    label: "$$$",
+    tooltip: "pricey",
+    minPerPerson: 21,
+    maxPerPerson: 30,
+  },
+];
+
+console.log(priceRange);
+
