bee98ad13033233a441b4cd764824da051c064b7
Your <ckiroshan@gmail.com>
2024-04-29 21:59:13 +0530

    Filtering arrays.

---
+++ Arrays/12. Filtering-arrays.js
@@ -0,0 +1,7 @@
+console.log("<== Filtering Arrays ==>");
+
+const numbers = [5, 7, 1, -30, 9, 8, -1];
+
+// Filter array with only positive numbers
+const filtered = numbers.filter((value) => value >= 0);
+
