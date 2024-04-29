3b0d7629e52a76fc9f9ef8339603cfb78225dfa6
Your <ckiroshan@gmail.com>
2024-04-29 16:53:28 +0530

    Check if atleast One Positive.

--- Arrays/11. Check-arrays.js
+++ Arrays/11. Check-arrays.js
@@ -1,6 +1,6 @@
 console.log("<== Checking Arrays ==>");
 
-const numbers = [5, 7, 1, 3, 9, 8];
+const numbers = [5, 7, 1, 3, 9, 8,-1];
 
 // Check if all numbers are positive
 const allPositiveNumbers = numbers.every((value) => {
@@ -8,4 +8,11 @@ const allPositiveNumbers = numbers.every((value) => {
 });
 
 console.log(allPositiveNumbers)
+
+// Check if at least 1 number is positive
+const atleastOnePositive = numbers.some((value) => {
+  return value >= 0;
+});
+
+console.log(atleastOnePositive)
 
