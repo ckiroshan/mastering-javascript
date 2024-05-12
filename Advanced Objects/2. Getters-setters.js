2b1d359a2455b50f56e3c8f19a303ca6db162e0c
Your <ckiroshan@gmail.com>
2024-05-12 15:10:16 +0530

    Getters and setters on objects.

---
+++ Advanced Objects/2. Getters-setters.js
@@ -0,0 +1,34 @@
+console.log("<=== Getters & Setters ===>");
+
+function Circle(radius) {
+  this.radius = radius;
+  let defaultLocation = { x: 0, y: 0 };
+
+  // Getter method
+  this.getDefaultLocation = function () {
+    return defaultLocation;
+  };
+
+  this.draw = function () {
+    console.log("draw!");
+  };
+
+  Object.defineProperty(this, "defaultLocation", {
+    get: function () {
+      return defaultLocation;
+    },
+
+    set: function (value) {
+      if (!value.x || !value.y) {
+        throw new Error("Invalid default location!");
+      }
+      defaultLocation = value;
+    },
+  });
+}
+
+const circle = new Circle(10);
+circle.draw();
+circle.defaultLocation = 1; // Error
+circle.defaultLocation = {x: 10, y: 10};
+console.log(circle.defaultLocation); // x: 10, y: 10
