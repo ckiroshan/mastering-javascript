e8a2a3d038b57804da8d731a289fadb336b61904
Your <ckiroshan@gmail.com>
2024-05-16 11:14:04 +0530

    Used WeakMaps dictionary to make private properties & methods.

---
+++ ES6/5. WeakMaps.js
@@ -0,0 +1,21 @@
+console.log("<=== Private properties using WeakMaps ===>");
+
+const _radius = new WeakMap();
+const _move = new WeakMap();
+
+class Circle {
+  constructor(radius) {
+    _radius.set(this, radius);
+
+    _move.set(this, () => {
+      console.log("move!", this);
+    });
+  }
+
+  draw() {
+    console.log(_radius.get(this));
+    _move.get(this)();
+  }
+}
+
+const c = new Circle(1);
