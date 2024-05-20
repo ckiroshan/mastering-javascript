f1f831ddb1f4994cd49d7fc5a2c92d275bd8bd94
Your <ckiroshan@gmail.com>
2024-05-20 23:28:48 +0530

    Used the DOM click event using addEventListener.

---
+++ DOM/4. DOM-events.js
@@ -0,0 +1,9 @@
+console.log("<=== DOM Events ===>");
+
+// Click
+const button = document.querySelector(".newButton");
+
+button.addEventListener("click", function () {
+  console.log("The Button has been clicked!");
+});
+
