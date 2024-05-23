04352bb5fe3d92b0b86172368eff2104ed1e982b
Your <ckiroshan@gmail.com>
2024-05-23 11:00:26 +0530

    Sent successful promises through functions.

---
+++ Asynchronous/3. Promises.js
@@ -0,0 +1,50 @@
+console.log("<=== Promises ===>");
+
+// Modern way of writing Asynchronous code.
+
+function registerUser() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Registration form is filled");
+      resolve();
+    }, 3000);
+  });
+}
+
+function sendOTP() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("One Time Password has been sent");
+      resolve();
+    }, 4000);
+  });
+}
+
+function login() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("User logged in successfully");
+      resolve();
+    }, 3000);
+  });
+}
+
+function getUserData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Getting User Data");
+      resolve();
+    }, 9000);
+  });
+}
+
+function DisplayUserData() {
+  setTimeout(() => {
+    console.log("Displaying User Data");
+  }, 8000);
+}
+
+console.log("===== Other processes =====");
+console.log("Loading Other User Data");
+
+registerUser().then(sendOTP).then(login).then(getUserData).then(DisplayUserData);
